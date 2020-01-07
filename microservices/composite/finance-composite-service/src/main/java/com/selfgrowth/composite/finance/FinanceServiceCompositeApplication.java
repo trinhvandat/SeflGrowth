0063eccd21beb1c5688a3b49abe0309e4ce27227
trinhvandat <trinhvandat90399@gmail.com>
2020-01-07 11:11:23 +0700

    update composite service

--- microservices/composite/finance-composite-service/src/main/java/com/selfgrowth/composite/finance/FinanceServiceCompositeApplication.java
+++ microservices/composite/finance-composite-service/src/main/java/com/selfgrowth/composite/finance/FinanceServiceCompositeApplication.java
@@ -1,2 +1,56 @@
 package com.selfgrowth.composite.finance;
 
+import org.apache.log4j.BasicConfigurator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@EnableSwagger2
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableAsync
+@ComponentScan({"com.selfgrowth.composite.finance", "com.selfgrowth.model.util"})
+
+public class FinanceServiceCompositeApplication{
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        ConfigurableApplicationContext context = SpringApplication.run(FinanceServiceCompositeApplication.class, args);
+    }
+
+    @Bean
+    public Docket swaggerPersonApi10(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.selfgrowth.composite.finance.service"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(new ApiInfoBuilder().version("1.0").title("Finance Composite Service").description("Documentation Finance API v1.0").build());
+    }
+}
